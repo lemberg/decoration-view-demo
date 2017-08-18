8f475ee76feea4345992695b007b9652c21a9ff7
Marian Fedyk <marian.fedyk@lemberg.co.uk>
2017-08-18 16:46:00 +0300

    Add several decorations;

--- DecorationViews/classes/CalendarFlowLayout.swift
+++ DecorationViews/classes/CalendarFlowLayout.swift
@@ -16,10 +16,17 @@ protocol CalendarFlowLayoutDelegate: UICollectionViewDelegateFlowLayout {
 class CalendarFlowLayout: UICollectionViewFlowLayout {
   
   var delegate: CalendarFlowLayoutDelegate!
-  var numberOfColumns = 2
   
   private let timeLine = "TimeLine"
-  private var titleRect : CGRect = CGRect(x: 10, y: 0, width: 200, height: 50)
+  private let timeLineHeight: CGFloat = 10
+  private var eventHeight: CGFloat = 30
+  private var numberOfItems: Int {
+    return collectionView!.numberOfItems(inSection: 0)
+  }
+  
+  private var collectionViewWidth: CGFloat {
+    return collectionView!.bounds.width
+  }
   
   override init() {
     super.init()
@@ -30,73 +37,31 @@ class CalendarFlowLayout: UICollectionViewFlowLayout {
     fatalError("init(coder:) has not been implemented")
   }
   
-  /// Array of all attributes for cells
   private var cache = [UICollectionViewLayoutAttributes]()
   
-  
-  /// Need to save it because of func `collectionViewContentSize`
-  private var contentHeight: CGFloat  = 0.0
-  
-  var flowContentInset: UIEdgeInsets  {
-    return UIEdgeInsets(top: minimumLineSpacing, left: minimumInteritemSpacing, bottom: minimumLineSpacing, right: minimumInteritemSpacing)
-  }
-  
-  private var contentGeneralWidth: CGFloat {
-    return collectionView!.bounds.width //- (flowContentInset.left + flowContentInset.right)
-  }
-  
-  private var contentWidthWithoutSpacing: CGFloat {
-    return contentGeneralWidth - offsetGeneralWidth
-  }
-  
-  private var offsetGeneralWidth: CGFloat {
-    return minimumInteritemSpacing * CGFloat(numberOfColumns + 1)
+  private var decorationAttributes: [UICollectionViewLayoutAttributes] {
+    var attributes: [UICollectionViewLayoutAttributes] = []
+    for item in 0..<numberOfItems {
+      let indexPath = IndexPath(item: item, section: 0)
+      if let decatts = self.layoutAttributesForDecorationView(ofKind:self.timeLine, at: indexPath) {
+        attributes.append(decatts)
+      }
+    }
+    return attributes
   }
   
   override func prepare() {
     super.prepare()
-    
-    guard let collectionView = self.collectionView else { return }
-    
-    cache = [UICollectionViewLayoutAttributes]()
-    contentHeight = 0
-    
-    let columnWidth = contentWidthWithoutSpacing / CGFloat(numberOfColumns)
-    
-    let numberOfSections: NSInteger = collectionView.numberOfSections
-    
-    guard numberOfSections > 0 else {
-      return
-    }
-    
     if cache.isEmpty {
-      
-      let columnWidth = 80
-      var xOffset = [CGFloat]()
-      for column in 0 ..< numberOfColumns {
-        xOffset.append(CGFloat(column * columnWidth))
-      }
-      for item in 0 ..< collectionView.numberOfItems(inSection: 0) {
-        
-        let indexPath = IndexPath(row: item, section: 0)
-        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-        
-        attributes.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
-        cache.append(attributes)
-      }
+      cache.append(contentsOf: decorationAttributes)
     }
   }
   
-  
-  
-  override func layoutAttributesForDecorationView( ofKind elementKind: String, at indexPath: IndexPath) ->
-    UICollectionViewLayoutAttributes? {
-      
-      let atts = UICollectionViewLayoutAttributes(
-        forDecorationViewOfKind:timeLine, with:indexPath)
-      atts.frame = self.titleRect
-      atts.zIndex = -1
-      return atts
+  override func layoutAttributesForDecorationView( ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+    let atts = UICollectionViewLayoutAttributes(forDecorationViewOfKind:timeLine, with:indexPath)
+    atts.frame = CGRect(x: 0, y: CGFloat(indexPath.item) * (timeLineHeight + eventHeight), width: collectionViewWidth, height: timeLineHeight)
+    atts.zIndex = -1
+    return atts
   }
   
   
@@ -104,19 +69,10 @@ class CalendarFlowLayout: UICollectionViewFlowLayout {
     var layoutAttributes = [UICollectionViewLayoutAttributes]()
     
     for attributes in cache {
-      
       if attributes.frame.intersects(rect) {
         layoutAttributes.append(attributes)
       }
-      
-      if let decatts = self.layoutAttributesForDecorationView(
-        ofKind:self.timeLine, at: IndexPath(item: 0, section: 0)) {
-        if rect.contains(decatts.frame) {
-          layoutAttributes.append(decatts)
-        }
-      }
     }
-    
     
     return layoutAttributes
   }
@@ -127,15 +83,8 @@ class CalendarFlowLayout: UICollectionViewFlowLayout {
     }
   }
   
-  override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
-    if (!(self.collectionView?.bounds.size.equalTo(newBounds.size))!) {
-      return true;
-    }
-    return false;
-  }
-  
   override var collectionViewContentSize: CGSize {
-    return CGSize(width: self.collectionView!.bounds.size.width, height: contentHeight)
+    return CGSize(width: collectionViewWidth, height: (timeLineHeight + eventHeight) * CGFloat(numberOfItems))
   }
   
 }
